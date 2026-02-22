8994397263aacd334b08c1af51c6143349299212
luis-fbs <baptistabaptista@discente.ufg.br>
2026-02-22 15:02:10 -0300

    Feature: create policy

--- TwinFramework/ditto.py
+++ TwinFramework/ditto.py
@@ -1,5 +1,3 @@
-from http.client import responses
-
 import requests
 
 class Ditto:
@@ -11,7 +9,7 @@ class Ditto:
     def set_basic_auth(self, user, password):
         self.basic_auth = (user, password)
 
-    def crete_thing(self, attributes, features, namespace=None, name=None, policy_id=None):
+    def create_thing(self, attributes, features, namespace=None, name=None, policy_id=None):
         url = f"http://{self.host}:{self.port}/api/2/things" #todo: TLS on Ditto to use HTTPS
         payload = {
             "attributes": attributes,
@@ -25,4 +23,14 @@ class Ditto:
             response = requests.put(url, json=payload, auth=self.basic_auth or None)
         else:
             response = requests.post(url, json=payload, auth=self.basic_auth or None)
-        return response.status_code in (201, 204)
+        return response.status_code, response.text
+
+    def create_policy(self, entries, policy_id=None):
+        url = f"http://{self.host}:{self.port}/api/2/policies"
+        if policy_id:
+            url += f"/{policy_id}"
+            response = requests.put(url, json=entries, auth=self.basic_auth or None)
+        else:
+            response = requests.post(url, json=entries, auth=self.basic_auth or None)
+        return response.status_code, response.text
+
