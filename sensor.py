9ba192324aaf91f5b89da25f299f542e2893b8fa
luis-fbs <luis-fbs@outlook.com>
2026-03-04 18:55:25 -0300

    Feature: define sensor publish behavior

--- sensor.py
+++ sensor.py
@@ -1,6 +1,9 @@
+import asyncio
+
 class Sensor:
-    def __init__(self, id, sync_rate):
+    def __init__(self, id, topics, sync_rate):
         self.id = id
+        self.topics = topics
         self.sync_rate = sync_rate
         self.get_sensor_data = None
 
@@ -8,3 +11,10 @@ class Sensor:
         if not self.get_sensor_data:
             raise NotImplementedError("get_sensor_data not implemented")
         return self.get_sensor_data()
+
+    async def publish_data(self, client, qos):
+        while True:
+            data = self.get_sensor_data()
+            for topic in self.topics["data"]:
+                await client.publish(topic, data, qos)
+            await asyncio.sleep(self.sync_rate)
